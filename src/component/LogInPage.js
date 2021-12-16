23299079c7e44a39bc565c6b6af9d016fedf0504
Andrea <ngocanhhsgs@gmail.com>
2021-12-16 11:41:56 +0200

    add file

---
+++ src/component/LogInPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+  Button,
+  Image,
+} from "react-native";
+
+function LogInPage() {
+  return (
+    <View>
+      <Text>Welcome to ADN !</Text>
+      <Text> Log in</Text>
+      <StatusBar style="auto" />
+      <TextInput> Email/ User Name</TextInput>
+      <TextInput> Password</TextInput>
+      <Button title="Log In"> </Button>
+      <Image source={require("../../assets/mainPage.jpeg")}></Image>
+    </View>
+  );
+}
+
+export default LogInPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
